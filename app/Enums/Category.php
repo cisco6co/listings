d21c8444bc2bdc879d7aab862390f24c34a3b097
Francisco Edouard <webifypeople@gmail.com>
2020-08-12 00:33:56 +0400

    Issue#1

---
+++ app/Enums/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * Class Category
+ *
+ * @package App\Enums
+ */
+final class Category extends Enum
+{
+    const FURNITURE   = 'Furniture';
+    const ELECTRONICS = 'Electronics';
+    const CARS        = 'Cars';
+    const PROPERTY    = 'Property';
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Auth::routes();
+
+Route::get('/', 'ListingController@index')->name('listings');
+Route::get('/{id}', 'ListingController@show')->name('listing');
+//Route::get('/home', 'HomeController@index')->name('home');
+
+
